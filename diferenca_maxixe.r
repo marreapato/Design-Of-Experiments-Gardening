d997049d39ad3ba8bfe09173d681e586e005497f
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2022-11-28 19:44:06 -0300

    Create diferenca_maxixe.r

---
+++ diferenca_maxixe.r
@@ -0,0 +1,20 @@
+#install.packages("WDI")
+library(tidyverse)
+
+dados <- read.csv("dadosexp - placa_maxixe.csv")
+
+summary(dados)
+
+dados$trat <- factor(dados$trat)
+
+boxplot(dados$alt_aprox~dados$trat,main="Crescimento do Maxixe Por Tratamento",ylab="Altura (cm)",xlab="Tratamento")
+
+boxplot(dados$folhas~dados$trat,main="Crescimento de folhas Maxixe Por Tratamento",ylab="N de Folhas",xlab="Tratamento")
+
+kruskal.test(dados$alt_aprox~dados$trat)
+
+kruskal.test(dados$folhas~dados$trat)
+
+pairwise.wilcox.test(dados$alt_aprox, g = dados$trat, p.adjust.method = "bonferroni")
+
+pairwise.wilcox.test(dados$folhas, g = dados$trat, p.adjust.method = "bonferroni")
